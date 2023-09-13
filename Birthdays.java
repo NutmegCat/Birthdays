2fba02c5fad9c53d7acfc840035e05bf2c5347c0
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-09-13 11:11:16 -0400

    sum 1 sum 2 vars

--- Birthdays.java
+++ Birthdays.java
@@ -3,20 +3,24 @@ class Birthdays{
         countTerms();
     }
 
-    // Calculates the amount of terms there are {#565, 15}
+    // Calculates the amount of terms there are {#565, 20}
     public static void countTerms () {
        
         final int DAYS = 365;
         int days = 365;
         int count = 0; // amount of (n/365)
         int term = 1;
+        double sum1 = 1;
+        double sum2 = 1;
 
         for (int i = 0; i < DAYS; i++) {
             double numerator = days - i;
             double fraction = numerator / 365;
             count++;
             numerator = numerator - term;
-            System.out.println(fraction + " " + count);
-        }
+            sum1 = 1 - fraction;
+            sum2 *= fraction;
+            System.out.println(fraction + " " + count + " " + sum2);
+        }  
     }
 }
