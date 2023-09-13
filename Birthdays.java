a8af64a87eeafc7f93db2cac8aa942a02ae170c4
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-09-13 17:52:19 -0400

    a thing that works???

--- Birthdays.java
+++ Birthdays.java
@@ -1,19 +1,28 @@
 class Birthdays {
     public static void main(String[] args) {
-        int[] n = new int[60];
-        double[] p = new double[60];
+        calculateAndPrintThresholds();
+    }
 
-        for (int i = 1; i <= 60; i++) {
-            double q = 1.0;
-            for (int j = 0; j < i; j++) {
-                q *= 1 - (double) j / 365;
+    public static void calculateAndPrintThresholds() {
+        final int DAYS = 365;
+        double[] thresholds = { 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 1.0 };
+        int[] requiredPeople = new int[thresholds.length];
+
+        for (int i = 0; i < thresholds.length; i++) {
+            int count = 0;
+            double probability = 1.0;
+
+            while (probability > 1.0 - thresholds[i]) {
+                probability *= (double) (DAYS - count) / DAYS;
+                count++;
             }
-            p[i - 1] = 1 - q;
+
+            requiredPeople[i] = count;
         }
 
-        // Print the result
-        for (int i = 0; i < 60; i++) {
-            System.out.println("p[" + i + "] = " + p[i]);
+        // Print the results
+        for (int i = 0; i < thresholds.length; i++) {
+            System.out.println("For threshold " + (thresholds[i] * 100) + "%, " + requiredPeople[i] + " people are required.");
         }
     }
 }
