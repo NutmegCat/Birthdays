c7c1d832a47d21e87ad9629f9820747b5d69fdd0
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-09-13 16:40:16 -0400

    changed var

--- Birthdays.java
+++ Birthdays.java
@@ -6,16 +6,16 @@ class Birthdays{
     // Calculates the amount of terms there are
     public static void countTerms () {
        
-        double dec1 = 0;
-        double dec2 = 0;
-        double dec3 = 0;
-        double dec4 = 0;
-        double dec5 = 0;
-        double dec6 = 0;
-        double dec7 = 0;
-        double dec8 = 0;
-        double dec9 = 0;
-        double dec10 = 0;
+        double number1 = 0;
+        double number2 = 0;
+        double number3 = 0;
+        double number4 = 0;
+        double number5 = 0;
+        double number6 = 0;
+        double number7 = 0;
+        double number8 = 0;
+        double number9 = 0;
+        double number10 = 0;
 
         final int DAYS = 365;
         int days = 365;
@@ -34,34 +34,34 @@ class Birthdays{
             // System.out.println(fraction + " " + count + " " + sum1 + " " + sum2);
             
             if (sum1 <= 0.1) {
-                dec1 = count;
+                number1 = count;
             } else {
                 if (sum1 <= 0.2) {
-                    dec2 = count;
+                    number2 = count;
                 } else {
                     if (sum1 <= 0.3) {
-                        dec3 = count;
+                        number3 = count;
                     } else {
                         if (sum1 <= 0.4) {
-                            dec4 = count;
+                            number4 = count;
                         } else {
                             if (sum1 <= 0.5) {
-                                dec5 = count;
+                                number5 = count;
                             } else {
                                 if (sum1 <= 0.6) {
-                                    dec6 = count;
+                                    number6 = count;
                                 } else {
                                     if (sum1 <= 0.7) {
-                                        dec7 = count;
+                                        number7 = count;
                                     } else {
                                         if (sum1 <= 0.8) {
-                                            dec8 = count;
+                                            number8 = count;
                                         } else {
                                             if (sum1 <= 0.9) {
-                                                dec9 = count;
+                                                number9 = count;
                                             } else {
                                                 if (sum1 <= 0.10) {
-                                                    dec10 = count;
+                                                    number10 = count;
                                                 } else {
                                                     ;
                                                 }
@@ -75,7 +75,6 @@ class Birthdays{
                 }
             }
         }  
-
-        System.out.println(dec1 + " " + dec2 + " " + dec3 + " " + dec4 + " " + dec5 + " " + dec6 + " " + dec7 + " " + dec8 + " " + dec9 + " " + dec10);
+        System.out.println(number1 + " " + number2 + " " + number3 + " " + number4 + " " + number5 + " " + number6 + " " + number7 + " " + number8 + " " + number9 + " " + number10);
     }
 }
