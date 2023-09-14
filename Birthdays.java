ca47a4ca9d08112a2668ec21e63f7f325ba0b9e2
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-09-14 10:18:39 -0400

    cleaned

--- Birthdays.java
+++ Birthdays.java
@@ -2,16 +2,16 @@
 
 class Birthdays {
     public static void main(String[] args) {
-        calculateAndPrintThresholds();
-    }
-
-    public static void calculateAndPrintThresholds() {
+        // days constant
         final int DAYS = 365;
-        double[] thresholds = { 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 1.0 };
+
+        // arrays for the percentages and people
+        double[] thresholds = {0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 1.0};
         int[] requiredPeople = new int[thresholds.length];
 
+        // for loop
         for (int i = 0; i < thresholds.length; i++) {
-            int count = 0;
+            int count = 0; // counter
             double probability = 1.0;
 
             while (probability > 1.0 - thresholds[i]) {
@@ -21,7 +21,7 @@ class Birthdays {
             requiredPeople[i] = count;
         }
 
-        // Print the results
+        // print the results
         for (int i = 0; i < thresholds.length; i++) {
             System.out.println("For threshold " + (thresholds[i] * 100) + "%, " + requiredPeople[i] + " people are required.");
         }
